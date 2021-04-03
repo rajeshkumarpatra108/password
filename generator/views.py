34a27b344047838423ff1992e612d65a9d3765ef
rajeshkumarpatra108 <rajeshpatra744@gmail.com>
2021-04-04 00:06:13 +0530

    push

---
+++ generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request,'generator/home.html')
+
+def password(request):
+
+
+    characters = list('abcdefghijklomnopqstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('uppercase'):
+        characters.extend(list('!@#$%^&*()'))
+
+
+
+    length = int(request.GET.get("length",12))
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render(request,'generator/password.html',{'password':thepassword})
